49b38e17e6d0f0308d157abf13c438dd9e24be66
Sarah Fopma <s.fopma@gmail.com>
2024-04-08 16:09:09 +0200

    First commit

---
+++ src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
